767103bac58c3ff77cfb75bcfae67ea922351944
Perandre1 <135350804+Perandre1@users.noreply.github.com>
2024-03-11 12:21:31 -0300

    .

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,11 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+while () {
+    listaDeTeclas[0].onclick = tocaSomPom;
+}
