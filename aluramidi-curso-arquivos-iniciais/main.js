656a790f26e61177137a2e48e72ed3e695c3697a
Perandre1 <135350804+Perandre1@users.noreply.github.com>
2024-03-11 12:36:52 -0300

    .

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -4,8 +4,12 @@ function tocaSomPom () {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
-
-while () {
+while (contador < 9) {
     listaDeTeclas[0].onclick = tocaSomPom;
-}
+
+    contador = contador +1;
+
+    console.log()
+} 
